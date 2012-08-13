3bded4977ba426071be9b3fb550723d555a007e8
Simonini <descovi@gmail.com>
2012-08-13 20:56:56 +0200

    Siamo pronti col compass.

--- header.php
+++ header.php
@@ -1,1 +1,6 @@
-testa
+<html>
+<head>
+<link href="<?php echo get_stylesheet_directory_uri(); ?>/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<link href="<?php echo get_stylesheet_directory_uri(); ?>/stylesheets/application.css" media="screen, projection" rel="stylesheet" type="text/css" />
+
+  </head>

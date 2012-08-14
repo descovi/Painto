c6c94811d27f92c349baebd726da11511ba19fa1
Simonini <descovi@gmail.com>
2012-08-14 07:21:07 +0200

    Integrato wp_nav_menu

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+// This theme uses Featured Images (also known as post thumbnails) for per-post/per-page Custom Header images
+add_theme_support( 'post-thumbnails' );
+
+// This theme uses wp_nav_menu() in one location.
+register_nav_menu( 'primary', __( 'Primary Menu', 'twentyeleven' ) );
+?>
--- header.php
+++ header.php
@@ -1,7 +1,35 @@
-<html>
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
 <head>
-  <title>Title of the document</title>
+<title><?php
+/*
+   * Print the <title> tag based on what is being viewed.
+   */
+global $page, $paged;
 
+wp_title( '|', true, 'right' );
+
+// Add the blog name.
+bloginfo( 'name' );
+
+// Add the blog description for the home/front page.
+$site_description = get_bloginfo( 'description', 'display' );
+if ( $site_description && ( is_home() || is_front_page() ) )
+  echo " | $site_description";
+
+?></title>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
 <link href="<?php echo get_stylesheet_directory_uri(); ?>/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
 <link href="<?php echo get_stylesheet_directory_uri(); ?>/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
   <!--[if IE]>
@@ -9,29 +37,25 @@
   <![endif]-->
   </head>
   <body class="default">
+    <div id="extra-decoration"></div>
 
 <header id="header" class="header">
   <div class="header-content">
     <a id="logo" class="logo">
       Painto.net
     </a><!--#logo-->
-    <div id="menu" class="menu">
-      <div id="portfolio" class="element">
-        <a href="./portfolio">portfolio</a>
-      </div>
-      <div id="about" class="element">
-        <a href="./about">about</a>
-      </div>
-      <div id="resume" class="element">
-        <a href="./resume">resume</a>
-      </div>
-      <div id="contact" class="element">
-        <a href="./contact">contact</a>
-      </div>
-    </div>
+    <nav id="access" role="navigation">
+
+      <?php 
+      $defaults = array(
+  'theme_location'  => 'primary',
+  'items_wrap'      => '<div class="menu">%3$s</div>',
+
+); ?>
+      <?php 
+      wp_nav_menu($defaults); ?>
+    </nav><!-- #access -->
   </div><!--.header-content-->
 </header><!--#header-->
 
 <div class="content">
-
-  asdffasdfdsa

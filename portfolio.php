49b1d1fecf0cbb00ca1bd2a2159ae6e4c3e0265f
Simonini <descovi@gmail.com>
2012-08-15 18:02:52 +0200

    Visualizzazione dei box dei progetti.

--- portfolio.php
+++ portfolio.php
@@ -2,18 +2,22 @@
 /*
 Template Name: Portfolio
 */
+get_header();
+?>
+<div class="portfolio">
+<?php
 
-//get_header();
 $the_query = new WP_Query('post_type=proggeto'); 
 //The loop
 while ( $the_query->have_posts() ) : $the_query->the_post();
-  the_title();
-  echo '<img src="';
-  echo get('img_images');
-  echo '">';
-endwhile;
+  ?>
+  <div class="box">
+    <a href="<?php the_permalink() ?>">
+    <div class="title_box"><h3 class="title"><?php the_title() ?></h3></div>
+    <div class="img" style="background-image:url('<?php echo get('img_images') ?>')" ></div>
+    </a>
+  </div>
+<? endwhile ?>
+</div>
+<?php wp_reset_postdata() ?>
 
-wp_reset_postdata();
-
-//get_footer();
-?>

70a0502e0983054d73bd770bf2b5804693bd3104
Simonini <descovi@gmail.com>
2012-08-30 15:48:32 +0200

    Recuperate la cover img.

--- portfolio.php
+++ portfolio.php
@@ -14,10 +14,11 @@ while ( $the_query->have_posts() ) : $the_query->the_post();
   <div class="box">
     <a href="<?php the_permalink() ?>">
     <div class="title_box"><h3 class="title"><?php the_title() ?></h3></div>
-    <div class="img" style="background-image:url('<?php echo get('img_images') ?>')" ></div>
+    <div class="img" style="background-image:url('<?php echo get('cover_images') ?>')" ></div>
     </a>
   </div>
 <? endwhile ?>
 </div>
 <?php wp_reset_postdata() ?>
 
+<?php get_footer() ?>

7540571588d4cd8bc75b689e5e81b0c1f8146937
Simonini <descovi@gmail.com>
2012-10-07 17:04:25 +0200

    Inserimento google analytics.

--- footer.php
+++ footer.php
@@ -4,5 +4,18 @@
       All images on this site are Copyright © 2012 Gabriele Pasqualino or their respective copyright holders. Do not use without permission.
     </div><!--.footer-content-->
   </footer>
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-440601-10']);
+  _gaq.push(['_trackPageview']);
+
+    (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                })();
+
+</script>
 </body>
 </html>
